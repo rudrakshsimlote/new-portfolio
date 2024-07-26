b0cdd2ff47fef9f52fbeb90fd96468caf55d2f43
Rudraksh <65094659+rudrakshsimlote@users.noreply.github.com>
2024-07-26 05:24:49 -0400

    Updated logo and other changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import { logo } from '../../assets/index';
-import { navLinksdata } from '../../constants';
-import { Link } from 'react-scroll';
+import { navLinksdata } from "../../constants";
+import { Link } from "react-scroll";
 import resume from '../../assets/resume_test.pdf';
+import logo from '../../assets/logo.png';  // Update this line to import your new logo
 
 const Navbar = () => {
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
-        <img src={logo} alt="Logo" />
+        <img src={logo} alt="Logo" className="w-12 h-12 rounded-full object-cover" />  {/* Update className to style the logo */}
       </div>
       <div>
         <ul className="flex items-center gap-10">
-          {navLinksdata.map(({ _id, title, link }) => (
-            <li
-              className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
-              key={_id}
-            >
-              {link === 'resume' ? (
-                <a href={resume} target="_blank" rel="noopener noreferrer">
-                  {title}
-                </a>
-              ) : (
+          {
+            navLinksdata.map(({_id, title, link}) => (
+              <li className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300" key={_id}>
                 <Link
                   activeClass="active"
                   to={link}
@@ -32,13 +25,13 @@ const Navbar = () => {
                 >
                   {title}
                 </Link>
-              )}
-            </li>
-          ))}
+              </li>
+            ))
+          }
         </ul>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Navbar;
